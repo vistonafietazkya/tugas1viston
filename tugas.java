297f551b734f64a25ab4642730147ab30dfbf75b
vistonafietazkya <vistonafietazkya@gmail.com>
2021-04-03 12:11:08 +0800

    Menambahkan variabel npm

--- tugas.java
+++ tugas.java
@@ -8,13 +8,16 @@ System.out.println("ini program tugas");
  Scanner scanner = new Scanner(System.in);
 
 	String tugas,jurusan;
+	int npm;
 
 System.out.print("Masukkan Matkul Anda: ");
 tugas= scanner.nextLine(); 
 System.out.print("Masukkan Jurusan Anda: ");
 jurusan= scanner.nextLine();
+System.out.print("Masukkan NPM Anda: ");
+npm = scanner.nextInt();
 System.out.println("Matkul Anda : "+tugas);
 System.out.println("Jurusan Anda :"+jurusan);
-
+System.out.println("Npm Anda :"+npm);
  }
 }
